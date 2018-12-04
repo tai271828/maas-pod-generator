0eaaf6481d8eaf30d3426bb8632df06b43786947
Taihsiang Ho (tai271828) <tai271828@gmail.com>
2018-12-04 21:12:10 +0800

    Apply more ingredients

--- mpg/launcher/mpg_cli.py
+++ mpg/launcher/mpg_cli.py
@@ -1,19 +1,48 @@
 import sys
 from guacamole import Command
+from guacamole.core import Ingredient
+from guacamole.ingredients import argparse
+from guacamole.ingredients import cmdtree
+from guacamole.recipes.cmd import CommandRecipe
 from mpg.launcher.launcher import Launcher
 from mpg.launcher.pod import MPGPod
 
-class MPGApplication(Command):
-    def invoked(self, ctx):
-        print('application subcommand is called.')
+
+class LauncherIngredient(Ingredient):
+    """Ingredient that adds MPG Launcher support to guacamole."""
+    def late_init(self, context):
+        print('LauncherIngredient is called. {}'.format(context))
+
+
+class MPGCommandRecipe(CommandRecipe):
+
+    """A recipe for using MPG-enhanced commands."""
+
+    def get_ingredients(self):
+        return [
+            cmdtree.CommandTreeBuilder(self.command),
+            cmdtree.CommandTreeDispatcher(),
+            argparse.ParserIngredient(),
+            LauncherIngredient()
+        ]
 
 
 class MPGCommand(Command):
+    """
+    A command with MPG-enhanced ingredients.
+
+    If no command is givin, launcher command is assumed.
+    See mpg-cli launcher -h for more information.
+    """
+
     sub_commands = (
         ('launcher', Launcher),
-        ('application', MPGApplication),
         ('pod', MPGPod)
     )
+
+    def main(self, argv=None, exit=True):
+        return MPGCommandRecipe(self).main(argv, exit)
+
 
 def main():
     known_cmds = [x[0] for x in MPGCommand.sub_commands]
