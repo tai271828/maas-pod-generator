6f4260cd97941f8a2bc9c250042a3e4af482a594
Taihsiang Ho (tai271828) <tai271828@gmail.com>
2018-12-06 10:17:36 +0800

    Read config in yaml

--- csgen/impl/secure/config.py
+++ csgen/impl/secure/config.py
@@ -1,6 +1,25 @@
+import yaml
+
+
 class Config:
+
+    def __init__(self):
+        """
+        Initialize an empty Config object
+        """
+        self.string = ''
+        self.config_yaml = None
+
     def read(self, filename_list):
         print('TODO: will be implemented later to merge multiple conf files.')
+        # as a prototype we use yaml for now
+        # maybe we should migrate to launcher conf later
+        self.config_yaml = self.read_yaml(self.string)
+
     def read_string(self, string):
         self.string = string
         print('Reading conf file...')
+
+    def read_yaml(self, stream):
+        return yaml.load(stream)
+
--- mpg/launcher/launcher.py
+++ mpg/launcher/launcher.py
@@ -1,16 +1,24 @@
 import gettext
 from guacamole import Command
 
+
 _ = gettext.gettext
+
 
 class Launcher(Command):
 
     name = 'launcher'
 
+    def __init__(self):
+        self.launcher = None
+        super().__init__()
+
     def register_arguments(self, parser):
         parser.add_argument(
             'launcher', nargs='?',
             help=_("Launcher definition file to use"))
+
     def invoked(self, ctx):
         self.launcher = ctx.cmd_toplevel.launcher
         print("The launcher is created by {}".format(ctx.args.launcher))
+        print("The config content is \n{}".format(self.launcher.config_yaml))
